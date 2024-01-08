ad666bb886d6a768549c66fbbdef729ade1fbe66
hoseinkhani <hessamhoseinkhani@gmail.com>
2024-01-08 20:19:48 +0100

    feat: add 404 page.

---
+++ src/app/_components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  styleUrl: './page-not-found.component.scss',
+  templateUrl: './page-not-found.component.html',
+  standalone: true
+})
+export class PageNotFoundComponent {
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import {Routes} from '@angular/router';
 import {DrinksListComponent} from "./_components/drinks-list/drinks-list.component";
 import {DrinkDetailsComponent} from "./_components/drink-details/drink-details.component";
+import {PageNotFoundComponent} from "./_components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   {path: '', component: DrinksListComponent},
   {path: 'drink/:id', component: DrinkDetailsComponent},
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
