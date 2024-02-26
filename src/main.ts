3fdf88b13f1365efd243168674d5c2b3c280ec1c
sam hoseinkhani <hsmhoseinkhani@gmail.com>
2024-02-26 14:00:58 +0100

    test actions

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+const x = "MY_API_KEY";
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
